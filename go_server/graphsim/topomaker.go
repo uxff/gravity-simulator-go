d8fafd91afb189ddfd788de90dd35f29a9ef54be
uxff <uxff@qq.com>
2018-08-28 19:52:21 +0800

    update topomaker

--- go_server/graphsim/topomaker.go
+++ go_server/graphsim/topomaker.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	//"image/draw"
 	"flag"
+	"sync"
 	//"image/jpeg"
 	"image/png"
 	"log"
@@ -35,11 +36,8 @@ type WaterDot struct {
 	xPower   float32 // v2
 	yPower   float32 // v2
 	dir      float64
-	h        int   // 积水高度，产生积水不参与流动，流动停止 // v2将由水滴实体代替该变量
-	q        int   // 流量 0=无 历史流量    //
-	input    []int // 流入坐标    // v2将取消该变量
-	nextIdx  int
-	hasNext  bool    // 有下一个方向
+	h        int     // 积水高度，产生积水不参与流动，流动停止 // v2将由水滴实体代替该变量
+	q        int     // 流量 0=无 历史流量    //
 	dirPower float32 // v2
 }
 type FlowList struct {
@@ -84,7 +82,7 @@ func (this *FlowList) Init(x int, y int, w *WaterMap, maxlen int) {
 // 先处理场向量
 // 再注水流动
 
-// 预先处理每个点的场向量
+// 预先处理每个点的场向量  只计算地势的影响，不考虑流量的影响
 // 假设每个点都有一个场，计算出这个场的方向
 // Topomap is basic topomap
 // WaterMap is empty fields of all, to be inited
@@ -110,15 +108,12 @@ func (w *WaterMap) AssignVector(m *Topomap, ring int) {
 		}
 
 		// 四环 四环影响力是二环的1/16
-		// if ring >= 4 {
-		//}
 
 		if xPower != 0 || yPower != 0 {
 			w.data[idx].dirPower = w.data[idx].dirPower + 1.0 //应该是邻居落差
 			w.data[idx].dir = math.Atan2(float64(yPower), float64(xPower))
 			w.data[idx].xPower, w.data[idx].yPower = float32(math.Cos(w.data[idx].dir)), float32(math.Sin(w.data[idx].dir))
 		}
-
 	}
 }
 
@@ -129,211 +124,51 @@ func UpdateDroplet() {
 
 }
 
-// v2 将使用新洒水法
-// 随机洒水法：
-/*
-	随机在地图中选择点，并滴入一滴水，尝试计算流出方向(判断旁边的水流方向)
-	如果没有流出方向，水位+1；如果有流出方向，按方向滴入下一位置,本地水位-1
-	向WaterMap中的某个坐标注水
-	水尝试找一个流动方向 随机roll出一个方向
-	注水，选择方向，流动耦合
-    todo: 思路：多次计算，找出最合适的出口
-*/
-func (w *WaterMap) InjectWater(pos int, m *Topomap) bool {
-	//log.Println("START INJECT: pos=", pos)
-	if pos >= len(w.data) {
-		return false
-	}
-	var curX, curY int = pos % w.width, pos / w.height
-	// 水源流向关系
-	var curDot *WaterDot = &w.data[pos]
-
-	// 流量过量则退出，否则栈溢出
-	if curDot.q > 255 {
-		// 理应断开与上下游关系，产生积水
-		if curDot.hasNext {
-			if w.data[curDot.nextIdx].q > 250 {
-				log.Println("seems flow in circle, cut me->next, me=:", curDot)
-				curDot.h++
-				curDot.hasNext = false
-				for _, itsInputIdx := range curDot.input {
-					w.data[itsInputIdx].hasNext = false
-				}
-				curDot.input = []int{}
-			} else {
-				log.Println("none circle exceed occur? next q=", w.data[curDot.nextIdx].q)
-			}
-		}
-		log.Println("too much quantity me=", curDot)
-		return false
+func MakeDroplet(w *WaterMap) *Droplet {
+	idx := rand.Int() % len(w.data)
+	d := &Droplet{
+		x: float32(idx%w.width) + 0.5,
+		y: float32(idx/w.width) + 0.5,
 	}
 
-	curDot.q++
+	// w
+	mu := sync.Mutex{}
+	mu.Lock()
+	defer mu.Unlock()
 
-	// if have output, go output
-	if curDot.hasNext {
-		// 查看下一个dot的input中有没有me
-		var nextHasMe bool = false
-		for _, itsInputIdx := range w.data[curDot.nextIdx].input {
-			if itsInputIdx == pos {
-				nextHasMe = true
-				break
-			}
-		}
-		// 如果没有，则将me加入到它的input
-		if nextHasMe == false {
-			w.data[curDot.nextIdx].input = append(w.data[curDot.nextIdx].input, pos)
-		}
-		//w.data[curDot.nextIdx].h++
-		return w.InjectWater(curDot.nextIdx, m)
-		return true
+	w.data[idx].h++
+	w.data[idx].q++
+
+	return d
+}
+
+func (d *Droplet) Move(m *Topomap, w *WaterMap) {
+	// 是否有场
+	oldIdx := int(d.x) + int(d.y)*w.width
+	d.x += w.data[oldIdx].xPower
+	d.y += w.data[oldIdx].yPower
+
+	// 越界判断
+	if int(d.x) < 0 || int(d.x) > w.width || int(d.y) < 0 || int(d.y) > int(w.height) {
+		// stay origin place
+		log.Printf("droplet move out of bound. stop move.")
+		return
 	}
 
-	// try to find dir for flowing out
-	var theDir, rollDir float64 = curDot.dir, 0.0
-	var tx, ty, tIdx int
-	var allvx, allvy float64 = 0.0, 0.0
-	//curDot.x, curDot.y = float32(curX)+0.5, float32(curY)+0.5
-
-	//curDot.dir = avg(curDot.input.dir)
-	var hasDir bool
-	curDot.dir, hasDir = curDot.calcInputAvgDir(w)
-	if !hasDir {
-		curDot.dir = rand.Float64() * math.Pi * 2.0
+	newIdx := int(d.x) + int(d.y)*w.width
+	// 无力场，待在原地
+	if newIdx == oldIdx {
+		log.Printf("no field power. stay here.")
+		return
 	}
 
-	// 选择可继续流出的方向
-	for i := 0; i < 20; i++ {
-		// 查找较低的坑 优先流向低处
-		lowest, lowestVal := curDot.getLowestNeighbor(curDot.getNeighbors(w), m)
-		if lowest < int(m.data[pos]) {
-			// 低了按照此方向走
-			tx, ty = lowestVal.x, lowestVal.y
-			curDot.hasNext = true
-			tIdx = ty*m.width + tx
-			log.Println("lower first curDot,tx,ty=", curDot, tx, ty)
-			break
-		} else if lowest > int(m.data[pos])+curDot.h {
-			curDot.hasNext = false
-			curDot.dir += math.Pi
-			log.Println("desire to go out, continue to try, curDot,m.h,pos=", curDot, int(m.data[pos]), pos, ",lowest,lowestVal", lowest, lowestVal)
-			//break
-			continue
-		}
+	mu := sync.Mutex{}
+	mu.Lock()
+	defer mu.Unlock()
 
-		var pit1 float64 = rand.Float64() - rand.Float64()
-		rollDir = pit1 * pit1 * pit1 * (math.Pi/1.2 + float64(i)/10.0)
-		//rollDir = rand.Float64() * math.Pi * 2.0
-		theDir = rollDir + curDot.dir
-
-		allvx, allvy = (math.Cos(theDir)), (math.Sin(theDir))
-		// 碰到边界
-		tx, ty = int(float64(curDot.x)+allvx), int(float64(curDot.y)+allvy)
-		if tx < 0 || ty < 0 || int(tx) > w.width-1 || int(ty) > w.height-1 {
-			log.Println("over bound tx,ty:", tx, ty, "o=", curDot, "i=", i)
-			//continue
-			// 任其流出地图外，不再让其流回来
-			//curDot.h--
-			return false
-		}
-		if tx == curX && ty == curY {
-			// 内部移动后还在内部，将下次的基点延长
-			curDot.x, curDot.y = curDot.x+float32(allvx)/2, curDot.y+float32(allvy)/2
-			//log.Println("inner step")
-			continue
-		}
-		// 计算地形落差 地形较高 不允许流向高处
-		tIdx = ty*m.width + tx
-
-		assumeFall := (int(m.data[tIdx])*1 + w.data[tIdx].h) - (int(m.data[pos])*1 + curDot.h)
-		if assumeFall >= 1 {
-			curDot.dir += math.Pi
-			log.Println("flow up, fall,dot,tx,ty=", assumeFall, curDot, tx, ty, "i=", i)
-			continue
-		}
-
-		// 对方的nextIdx不能是me
-		if w.data[tIdx].hasNext && w.data[tIdx].nextIdx == pos {
-			//continue
-			// 撤销对方指向me的next，如果我方地形高
-			if assumeFall < 0 {
-				log.Println("discard target next direct me,target=", curDot, w.data[ty*m.width+tx])
-				w.data[tIdx].hasNext = false
-			} else {
-				log.Println("cannot flow to target because its next is me: me=", curDot, "target=", w.data[ty*m.width+tx], "i=", i)
-				continue
-			}
-		}
-
-		// 不能交叉 如果target是斜对面，相邻的不能是next关系
-		crossM := (tx - curX) * (ty - curY)
-		if crossM == 1 || crossM == -1 {
-			// 流向斜对面
-			near1x, near1y := tx, curY
-			near2x, near2y := curX, ty
-			if w.data[near1x+near1y*w.width].hasNext && w.data[near1x+near1y*w.width].nextIdx == (near2x+near2y*w.width) {
-				// 下游是2
-				log.Println("become cross:x,y,tx,ty=", curX, curY, tx, ty, "redir to x,y=", near2x, near2y)
-				tx, ty = near2x, near2y
-			} else if w.data[near2x+near2y*w.width].hasNext && w.data[near2x+near2y*w.width].nextIdx == (near1x+near1y*w.width) {
-				// 下游是1
-				log.Println("become cross:x,y,tx,ty=", curX, curY, tx, ty, "redir to x,y=", near1x, near1y)
-				tx, ty = near1x, near1y
-			}
-			tIdx = ty*m.width + tx
-		}
-
-		curDot.hasNext = true
-		break
-	}
-
-	// 选择成功则流入
-	if curDot.hasNext {
-		log.Println("got dir ok: rollDir=", rollDir, "pos xy=", pos, curX, curY, "target x,y:", tx, ty, "curH,tarH=", m.data[pos], m.data[tIdx])
-		// 不能过于激进地修改方向，否则曲线太弯曲容易eatself
-		//		if curDot.dir-theDir < -math.Pi {
-		//			curDot.dir = (curDot.dir+theDir)/2.0 - math.Pi
-		//		} else if curDot.dir-theDir > math.Pi {
-		//			curDot.dir = -(curDot.dir+theDir)/2.0 - math.Pi
-		//		} else {
-		//			curDot.dir = curDot.dir + (curDot.dir-theDir)/2.0 //(curDot.dir + theDir) / 2.0
-		//		}
-		//curDot.dir = theDir
-		curDot.dir = curDot.dir + rollDir/2.0
-
-		curDot.nextIdx = tIdx //tx + w.width*ty
-		// 查看下一个dot的input中有没有me
-		var nextHasMe bool = false
-		for _, itsInputIdx := range w.data[curDot.nextIdx].input {
-			if itsInputIdx == pos {
-				nextHasMe = true
-				break
-			}
-		}
-		// 如果没有，则将me加入到它的input
-		if nextHasMe == false {
-			w.data[curDot.nextIdx].input = append(w.data[curDot.nextIdx].input, pos)
-			w.data[curDot.nextIdx].giveInputXY(curDot.x+float32(allvx), curDot.y+float32(allvy))
-		}
-		//w.data[curDot.nextIdx].h++
-		return w.InjectWater(curDot.nextIdx, m)
-	} else {
-		log.Println("cannot flow anywhere: curDot=", curDot)
-		// 积水
-		curDot.h++
-		// 切断curDot跟下游的关系 本来就是断的
-		//curDot.hasNext = false
-		// 切断curDot跟上游的关系
-		curDot.input = make([]int, 0)
-		for _, itsInputIdx := range curDot.input {
-			w.data[itsInputIdx].hasNext = false
-		}
-		// 反流 往上反馈此路不通 让上路重新选择
-		for _, itsInputIdx := range curDot.input {
-			w.InjectWater(itsInputIdx, m)
-		}
-	}
-	return false
+	w.data[oldIdx].h--
+	w.data[newIdx].h++
+	w.data[newIdx].q++
 }
 
 func (this *Topomap) Init(width int, height int) {
@@ -350,7 +185,7 @@ func (this *WaterMap) Init(width int, height int) {
 		for y := 0; y < this.height; y++ {
 			this.data[x+y*this.width].x = float32(x) + 0.5
 			this.data[x+y*this.width].y = float32(y) + 0.5
-			this.data[x+y*this.width].hasNext = false
+			//this.data[x+y*this.width].hasNext = false
 		}
 	}
 }
@@ -393,25 +228,6 @@ func lineTo(img *image.RGBA, startX, startY, destX, destY int, lineColor, startC
 	if startX != destX && startY != destY {
 		img.Set(startX+int(i/distM*float64(destX-startX)), startY+int(i/distM*float64(destY-startY)), startColor)
 	}
-}
-
-// 计算来源的平均夹角
-func (d *WaterDot) calcInputAvgDir(w *WaterMap) (dir float64, hasDir bool) {
-	for i, inputIdx := range d.input {
-		inputDot := &w.data[inputIdx]
-		if i == 0 {
-			dir = inputDot.dir
-			hasDir = true
-		} else {
-			// 合并方向 夹角大于180 取-平均值
-			if dir-inputDot.dir < -math.Pi || dir-inputDot.dir > math.Pi {
-				dir = -(dir + inputDot.dir) / 2.0
-			} else {
-				dir = (dir + inputDot.dir) / 2.0
-			}
-		}
-	}
-	return dir, hasDir
 }
 
 func (d *WaterDot) giveInputXY(inputX, inputY float32) {
@@ -739,10 +555,9 @@ func DrawToImg(img *image.RGBA, m *Topomap, w *WaterMap, maxColor float32, zoom 
 			//log.Println("hasNext:", dot, w.data[dot.nextIdx])
 
 			// 如果是源头 则绘制白色
-			if len(dot.input) == 0 {
-				//tmpColor = color.White
-				img.Set(int(dot.x)**zoom+*zoom/2, int(dot.y)**zoom+*zoom/2, color.White)
-			}
+			//if len(dot.input) == 0 {
+			//img.Set(int(dot.x)**zoom+*zoom/2, int(dot.y)**zoom+*zoom/2, color.White)
+			//}
 		}
 	}
 	// 绘制颜色模板
