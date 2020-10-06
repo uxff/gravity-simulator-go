2dd6368f08b4faf1e689c6f2c0585d9b17cad58f
uxff <uxff@qq.com>
2020-10-06 16:44:39 +0800

    fix repulsion

--- go_server/orbs/repulsion.go
+++ go_server/orbs/repulsion.go
@@ -2,7 +2,7 @@ package orbs
 
 const (
 	// RepulsionG 斥力常数
-	RepulsionG = G / 2.0
+	RepulsionG = G * 2
 )
 
 // 斥力与距离的三次方成反比
@@ -12,7 +12,7 @@ func (o *Orb) CalcRepulsion(target *Orb, dist float64) Acc {
 	var a Acc
 
 	// 万有斥力
-	a.A = target.Mass / (dist * dist * dist) * RepulsionG
+	a.A = target.Mass / (dist * dist * dist * dist) * RepulsionG
 	a.Ax = -a.A * (target.X - o.X) / dist
 	a.Ay = -a.A * (target.Y - o.Y) / dist
 	a.Az = -a.A * (target.Z - o.Z) / dist
